1254c5e58348f6db4a3976f6c7bd5e94dd4bb05f
Phuc Phan <phucphanthanhthien@gmail.com>
2025-08-28 21:55:08 +0700

    feat: migrate to angular 19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,12 @@ import {Component} from '@angular/core';
 import {YesNoButtonComponent} from './yes-no-button/yes-no-button.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    YesNoButtonComponent,
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    imports: [
+        YesNoButtonComponent,
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
--- src/app/home/yes-no-button/yes-no-button.component.ts
+++ src/app/home/yes-no-button/yes-no-button.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-yes-no-button',
-  standalone: true,
-  imports: [],
-  templateUrl: './yes-no-button.component.html',
-  styleUrl: './yes-no-button.component.css'
+    selector: 'app-yes-no-button',
+    imports: [],
+    templateUrl: './yes-no-button.component.html',
+    styleUrl: './yes-no-button.component.css'
 })
 export class YesNoButtonComponent {
   onYesClick() {
