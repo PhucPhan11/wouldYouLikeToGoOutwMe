582020a851d25900df76399a022dd7500a0e5e0e
TheDogtorhehe <phucphanthanhthien@gmail.com>
2026-07-28 15:04:50 +0700

    No button action

--- src/app/home/yes-no-button/yes-no-button.component.spec.ts
+++ src/app/home/yes-no-button/yes-no-button.component.spec.ts
@@ -20,4 +20,20 @@ describe('YesNoButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update the No label and button scales until the final label', () => {
+    component.onNoClick();
+
+    expect(component.noLabel).toBe('Are you sure?');
+    expect(component.yesScale).toBe(1.15);
+    expect(component.noScale).toBe(0.85);
+
+    for (let index = 0; index < 10; index++) {
+      component.onNoClick();
+    }
+
+    expect(component.noLabel).toBe('Yes, pleaseeeee');
+    expect(component.yesScale).toBe(1.75);
+    expect(component.noScale).toBe(0.25);
+  });
 });
--- src/app/home/yes-no-button/yes-no-button.component.ts
+++ src/app/home/yes-no-button/yes-no-button.component.ts
@@ -7,11 +7,36 @@ import { Component } from '@angular/core';
     styleUrl: './yes-no-button.component.css'
 })
 export class YesNoButtonComponent {
+  readonly noLabels = [
+    'No',
+    'Are you sure?',
+    'Honey please',
+    "Don't do this to me :(",
+    "I'm gonna cry ...",
+    'Yes, pleaseeeee'
+  ];
+
+  noLabelIndex = 0;
+
+  get noLabel(): string {
+    return this.noLabels[this.noLabelIndex];
+  }
+
+  get yesScale(): number {
+    return 1 + this.noLabelIndex * 0.15;
+  }
+
+  get noScale(): number {
+    return 1 - this.noLabelIndex * 0.15;
+  }
+
   onYesClick() {
     console.log('Yes clicked!');
   }
 
   onNoClick() {
-    console.log('No clicked!');
+    if (this.noLabelIndex < this.noLabels.length - 1) {
+      this.noLabelIndex++;
+    }
   }
 }
