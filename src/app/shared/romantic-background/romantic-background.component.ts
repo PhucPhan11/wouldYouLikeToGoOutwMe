1b144c00ae8ec95b48bbe31dd78cb167fdd1d0af
TheDogtorhehe <phucphanthanhthien@gmail.com>
2026-07-28 17:00:23 +0700

    refactor style

---
+++ src/app/shared/romantic-background/romantic-background.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-romantic-background',
+  imports: [],
+  templateUrl: './romantic-background.component.html'
+})
+export class RomanticBackgroundComponent {}
